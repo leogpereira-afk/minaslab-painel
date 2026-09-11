c49c53352570f1e22faf5c617d6ca2a2e2451d9c
leogpereira-afk <leogpereira@gmail.com>
2026-09-11 17:16:51 -0300

    Layout: carregar padrão global de tabelas

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./styles/tabelas-padrao.css";
 import "./nfse-layout.css";
 import "./styles/movimentacao-conta-responsive.css";
 
